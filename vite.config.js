117157761ba398398fb55b3edcc6016a3a42b0b9
ZhenyaMelnichuk <zhenyamlnck212@gmail.com>
2024-11-15 20:48:47 +0200

    fixed VITE_BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    base: process.env.VITE_BASE_URL
-})
+export default ({ mode }) => {
+    const config = loadEnv(mode, process.cwd())
+    return defineConfig({
+        plugins: [vue(), vueDevTools()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        base: config.VITE_BASE_URL || '/'
+    })
+}
