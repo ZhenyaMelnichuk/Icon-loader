b2d46db7748eb51c709012d7ccd31db60781afc1
ZhenyaMelnichuk <zhenyamlnck212@gmail.com>
2024-11-15 19:10:13 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <Header />
+    <main>
+        <RouterView />
+    </main>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <img alt="logo" class="logo" src="@/assets/logo.svg" />
+        <nav>
+            <router-link :to="{ name: 'Home' }"> Home </router-link>
+            <router-link :to="{ name: 'Main' }"> Main </router-link>
+        </nav>
+    </header>
+</template>
+
+<style lang="less" scoped>
+header {
+    display: flex;
+    align-items: center;
+    line-height: 1.5;
+    max-height: 100vh;
+
+    .logo {
+        width: 4rem;
+        margin-right: 2rem;
+    }
+
+    nav {
+        width: 100%;
+
+        a {
+            padding: 0 1rem;
+            border-left: 1px solid var(--color-border);
+
+            &:first-of-type {
+                border: 0;
+            }
+        }
+
+        .router-link-exact-active {
+            color: var(--color-text);
+        }
+    }
+}
+</style>
