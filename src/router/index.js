c3af0b8747eaf8939af2f168032a5f7d3db7e14f
ZhenyaMelnichuk <zhenyamlnck212@gmail.com>
2024-11-15 21:05:07 +0200

    chabged createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '@/views/Home.vue'
 import Main from '@/views/Main.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
